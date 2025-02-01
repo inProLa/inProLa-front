428f9b6ad7c1bac3b70233d17444485638f58db4
Daniel Correia <danncorreia95@hotmail.com>
2025-02-01 17:01:13 -0300

    adicionar variaveis de ambiente

--- src/app/search/search.service.ts
+++ src/app/search/search.service.ts
@@ -2,26 +2,27 @@ import {Injectable} from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {SearchResponse} from "./models/searchResponse";
 import {Observable} from "rxjs";
+import {environment} from '../../environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
-export class  SearchService {
+export class SearchService {
 
   constructor(private client: HttpClient) {}
 
   searchForAcademicWorks(searchValue: string, filters: Array<string>) {
     return this.client.get<Array<SearchResponse>>(
-      `http://localhost:3000/search?searchText=${searchValue}${this._buildFilters(filters)}`
+      `${environment.API_URL}search?searchText=${searchValue}${this._buildFilters(filters)}`
     );
   }
 
   downloadZIPAcademicWork(fileId: string) {
-    return this.client.get(`http://localhost:3000/search/download/zip?fileId=${fileId}`, { responseType: 'blob' });
+    return this.client.get(`${environment.API_URL}search/download/zip?fileId=${fileId}`, { responseType: 'blob' });
   }
 
   downloadPDFAcademicWork(fileId: string) {
-    return this.client.get(`http://localhost:3000/search/download/pdf?fileId=${fileId}`, { responseType: 'blob' });
+    return this.client.get(`${environment.API_URL}search/download/pdf?fileId=${fileId}`, { responseType: 'blob' });
   }
 
   uploadNovoTrabalho(title: string, file: File): Observable<any> {
@@ -29,11 +30,11 @@ export class  SearchService {
     formData.append('title', title);
     formData.append('file', file);
 
-    return this.client.post(`http://localhost:3000/process/upload`, formData)
+    return this.client.post(`${environment.API_URL}process/upload`, formData)
   }
 
   getFiltersNames(): Observable<string[]> {
-    return this.client.get<string[]>(`http://localhost:3000/search/filters`);
+    return this.client.get<string[]>(`${environment.API_URL}search/filters`);
   }
 
   _buildFilters(filters: string[]): string {
